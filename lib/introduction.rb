170246505659d3bbe7c63b9e0edf721b0043301f
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-03-22 22:02:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction(name)
-
 def introduction_with_language(name, langauage)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
